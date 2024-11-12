959da76bc6cb2f3934549a923272f6d2ce54b784
Raffael Mancini <raffael.mancini@mnhn.lu>
2024-11-12 16:50:08 +0100

    Actually load correctly and remove unused stuff

--- mdata.R
+++ mdata.R
@@ -1,3 +1,5 @@
+library(sf)
+
 load_data <- function(path) {
     mdata <- read.csv(path, encoding="latin1")
 
@@ -6,12 +8,12 @@ load_data <- function(path) {
     mdata <- mdata[complete.cases(mdata$Long),]
 
     # Keep only strict minimum
-    reduced <- mdata[,c("Observation_Key","Lat", "Long", "preferred")]
+    mdata <- mdata[,c("Observation_Key","Lat", "Long", "preferred")]
 
-    reduced <- reduced[, c(1,3,2,4)]
-    colnames(reduced) <- c("name", "lng", "lat", "species")
+    mdata <- mdata[, c(1,3,2,4)]
+    colnames(mdata) <- c("name", "lng", "lat", "species")
 
-    all_points <- st_as_sf(reduced, coords = c("lng", "lat"), crs = 4326)
+    all_points <- st_as_sf(mdata, coords = c("lng", "lat"), crs = 4326)
     all_points <- st_transform(all_points, 2169)
     all_points
 }
