5dbce33428da47ff514a550a5c14cc2d78e60638
Julian Wittische <Julian.Wittische@mnhn.lu>
2024-11-14 18:14:42 +0100

    coldspots + layer; first test

--- coldspots.R
+++ coldspots.R
@@ -9,19 +9,56 @@
 ################################################################################
 
 library(sf)
-library(eks)
+#library(eks)
 library(ggplot2)
 library(rgeoboundaries)
+library(spatstat)
+library(raster)
 
+lux_borders <- geoboundaries(country = "Luxembourg", adm_lvl = "adm0")
+lux_borders <- st_transform(lux_borders, crs = 2169)
+
+source("config.R")
+source("mdata.R")
+
+#contour_data <- load_data(DATA_PATH)
+contour_data <- contour_data["geometry"]
+contour_data <- st_crop(contour_data, lux_borders)
+
+
+
+
+# ##### SPATSTAT
+# 
+# ppp_points <- as.ppp(st_coordinates(contour_data), W = as.owin(st_bbox(contour_data)))
+# 
+# density_result <- density(ppp_points, sigma = 100)
+# 
+# density_raster <- raster(density_result)
+# 
+# contours <- rasterToContour(density_raster)
+# 
+# contour_sf <- st_as_sf(contours)
+# 
+# # Plot contours with sf
+# plot(st_geometry(contour_sf))
+
+
+
+##### EKS DOES NOT WORK FOR BIG NUMBERS ???
 # Create KDE
-skde <- st_kde(all_points)
+# all_pointsOLD <-all_points
+# skdeOLD <-skde
 
-##### Plotting
-# # km scale
-# gsc <- ggspatial::annotation_scale(data=all_points, location="br", width_hint=0.2, bar_cols=1)
+set.seed(2)
+skde <- st_kde(contour_data[sample(1:nrow(contour_data), 500000),], gridsize= c(100,100))
 
-contours <- st_get_contour(skde, cont=c(95, 96, 97, 98, 99, 99.99))
-contours <-  st_crop(contours, lux_borders_buff)
+# skde <- st_kde(contour_data, binned = TRUE, gridsize= c(10,10))
+# skde2 <- st_kde(contour_data, binned=TRUE)
+# skde_forced_multi <- skde 
+
+contours <- st_get_contour(skde, cont=c(85, 90, 96, 97, 98, 99, 99.999))
+contours <-  st_crop(contours, lux_borders)
 
 theme_set(ggthemes::theme_map())
 
@@ -29,37 +66,34 @@ theme_set(ggthemes::theme_map())
 # Actual kde
 gs <- ggplot(skde, stoke=NA) #+ geom_sf(data=lux_borders) #+ geom_sf(data=all_points) no points
 
-# Colors
-percentile_colors <- c(# Light blue, semi-transparent
-  "95%" = alpha("black", 0),
-  "96%" = alpha("blue", 0.1),        # Medium blue, more opaque
-  "97%" = alpha("blue", 0.2),   # Dark blue, mostly opaque
-  "98%" = alpha("blue", 0.3),
-  "99%" = alpha("blue", 0.4),        # Medium blue, more opaque
-  "99.999%" = alpha("blue", 0.5)    # Dark blue, mostly opaque
-)
 
 # Colors
 percentile_colors <- c(# Light purple, semi-transparent
-  "95%" = alpha("black", 0),
-  "96%" = alpha("purple4", 0.1),        # Medium purple, more opaque
-  "97%" = alpha("purple4", 0.2),   # Dark purple, mostly opaque
-  "98%" = alpha("purple4", 0.3),
-  "99%" = alpha("purple4", 0.4),        # Medium purple, more opaque
-  "99.999%" = alpha("purple4", 0.5)    # Dark purple, mostly opaque
+  "85%" = alpha("black", 0),
+  "90%" = alpha("purple4", 0.1),
+  "96%" = alpha("purple4", 0.3),        # Medium purple, more opaque
+  "97%" = alpha("purple4", 0.5),   # Dark purple, mostly opaque
+  "98%" = alpha("purple4", 0.8),
+  "99%" = alpha("purple4", 0),        # Medium purple, more opaque
+  "99.999%" = alpha("purple4", 0)    # Dark purple, mostly opaque - transparent because border effect
 )
 
-# Colors
-percentile_colors <- c(# Light purple, semi-transparent
-  "95%" = alpha("black", 0),
-  "96%" = alpha("orange", 0.1),        # Medium purple, more opaque
-  "97%" = alpha("orange", 0.2),   # Dark purple, mostly opaque
-  "98%" = alpha("orange", 0.3),
-  "99%" = alpha("orange", 0.4),        # Medium purple, more opaque
-  "99.999%" = alpha("orange", 0.5)    # Dark purple, mostly opaque
-)
+# Remove border effect
+lux_buf500 <- st_buffer(lux_borders, dist=-1500)
+cont_corr <- st_intersection(contours, lux_buf500)
 
 # Plot
 gs + geom_sf(data=contours, aes(fill=label_percent(contlabel)), color=NA) + 
   scale_fill_manual(values=percentile_colors) +
   ggthemes::theme_map()
+
+# Plot
+gs + geom_sf(data=cont_corr, aes(fill=label_percent(contlabel)), color=NA) + 
+  scale_fill_manual(values=percentile_colors) +
+  ggthemes::theme_map()
+
+st_write(cont_corr, "contours_1500.geojson")
+
+
+
+
